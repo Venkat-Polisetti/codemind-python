bffb6d0cb6c7af5be22c5bae3f0ef74b8a16fb5c
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-08 07:25:09 +0530

    t=int(input()) while t:     a=int(input())     temp=a     while True:         for i in range(2,int(a**0.5)+1):             if(a%i==0):                 break         else:             np=a             break         a+=1     a=temp     while a:         for i in range(2,int(a**0.5)+1):             if(a%i==0):                 break         else:             pp=a             break         a-=1     a=temp     if(abs(np-a)<abs(pp-a)):         print(np)     elif(abs(np-a)>abs(pp-a)):         print(pp)     else:         print(pp)     t-=1

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,27 +1,29 @@
 t=int(input())
-v1=0
-v2=0
-for s in range(t):
+while t:
     a=int(input())
-    b=a
     temp=a
-    while a>1:
-        for j in range(2,int(a**0.5)+1):
-           if a%j==0:
+    while True:
+        for i in range(2,int(a**0.5)+1):
+            if(a%i==0):
                 break
         else:
-         v1=a
-         break
-        a=a-1
-    while temp:
-        for j in range(2,int(temp**0.5)+1):
-            if temp%j==0:
-                break
-        else:
-            v2=temp
+            np=a
             break
-        temp+=1
-    if (abs(v1-b))>(abs(b-v2)):
-        print(v2)
+        a+=1
+    a=temp
+    while a:
+        for i in range(2,int(a**0.5)+1):
+            if(a%i==0):
+                break
+        else:
+            pp=a
+            break
+        a-=1
+    a=temp
+    if(abs(np-a)<abs(pp-a)):
+        print(np)
+    elif(abs(np-a)>abs(pp-a)):
+        print(pp)
     else:
-        print(v1)
+        print(pp)
+    t-=1
