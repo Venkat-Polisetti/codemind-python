28255f449e985ab0c95604b5952aa556bc13e06e
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-29 22:43:58 +0530

    m,n=map(int,input().split()) i=1 gcd=0 for i in range(1,m+1):     if n%i==0 and m%i==0:         gcd=i     i+=1 print(gcd)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,6 +1,8 @@
-a,b=map(int,input().split())
+m,n=map(int,input().split())
+i=1
 gcd=0
-for i in range(1,b+1):
-    if a%i==0 and b%i==0:
+for i in range(1,m+1):
+    if n%i==0 and m%i==0:
         gcd=i
-print(gcd)
+    i+=1
+print(gcd) 
