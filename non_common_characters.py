b9ea4ccbf9575379cac969db0329ccd1f6eea892
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-07-23 22:12:45 +0530

    a=input() b=input() a,b=a.lower(),b.lower() a,b=list(a),list(b) c=[] a,b=set(a),set(b) for i in a:     if i==' ':         continue     if i not in b:         c.append(i) for i in b:     if i==' ':         continue     if i not in a:         c.append(i) c.sort() for i in c:     print(i,end='')

---
+++ non_common_characters.py
@@ -0,0 +1,19 @@
+a=input()
+b=input()
+a,b=a.lower(),b.lower()
+a,b=list(a),list(b)
+c=[]
+a,b=set(a),set(b)
+for i in a:
+    if i==' ':
+        continue
+    if i not in b:
+        c.append(i)
+for i in b:
+    if i==' ':
+        continue
+    if i not in a:
+        c.append(i)
+c.sort()
+for i in c:
+    print(i,end='')
