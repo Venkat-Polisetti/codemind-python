80ff2ecaa670357ab364eced6ad906fa8c70da0a
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-07-30 14:37:49 +0530

    s=input() s=s.lower() s=s.split() c=0 for i in s:     for j in range(len(i)):         if i[j] in 'aeiou':           c+=1     print(c,end=' ')     c=0

--- vowel_count_in_each_word.py
+++ vowel_count_in_each_word.py
@@ -1,12 +1,10 @@
 s=input()
 s=s.lower()
 s=s.split()
-a=[]
 c=0
 for i in s:
+    for j in range(len(i)):
+        if i[j] in 'aeiou':
+          c+=1
+    print(c,end=' ')
     c=0
-    for j in i:
-        if j in 'aeiou':
-            c+=1
-    a.append(c)
-print(*a)
