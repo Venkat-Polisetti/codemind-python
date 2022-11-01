fa8b1aaea2093cfbdc655f19dfdc52b6cff1f8c2
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-11-01 22:16:32 +0530

    s=input() k='aeiouAEIOU' t="" for i in s:     if i in k and i not in t:         t+=i+" " if t==" ":     print("-1") else:     print(t.strip())

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,14 +1,10 @@
-a=input()
-a=list(a)
-e=[]
-d=[]
-for i in a:
-    if i not in e:
-        e.append(i)
-for i in e:
-    if i in "aeiouAEIOU":
-        d.append(i)
-if d==" ":
-    print(-1)
+s=input()
+k='aeiouAEIOU'
+t=""
+for i in s:
+    if i in k and i not in t:
+        t+=i+" "
+if t==" ":
+    print("-1")
 else:
-    print(*d)
+    print(t.strip())
