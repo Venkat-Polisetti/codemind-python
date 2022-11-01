0a24735fe63692990c25cc54e50dca5b567af517
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-11-01 22:19:44 +0530

    s=input() t="" k="" for i in s:     if i in 'AEIOU' and i not in t:         t+=i     if i in 'aeiou' and i not in k:         k+=i if len(k)==5 or len(t)==5:     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,15 +1,12 @@
 s=input()
-a=['a','e','i','o','u']
-b=['A','E','I','O','U']
-c=0
-d=0
-for i in a:
-    if i in s:
-        c+=1
-for i in b:
-    if i in s:
-        d+=1
-if c==5 or d==5:
-    print('True')
+t=""
+k=""
+for i in s:
+    if i in 'AEIOU' and i not in t:
+        t+=i
+    if i in 'aeiou' and i not in k:
+        k+=i
+if len(k)==5 or len(t)==5:
+    print("True")
 else:
-    print('False')
+    print("False")
