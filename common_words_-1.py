6a9342a920e5ea573bc0f0892c94faeeddf24be7
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-11-01 23:35:06 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() s1=s1.split() s2=s2.split() c=0 for i in s1:     if i in s2:         c+=1 print(c)

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,9 +1,11 @@
-a=input()
-b=input()
-a,b=a.lower(),b.lower()
-a,b=a.split(),b.split()
-a,b=list(a),list(b)
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+s1=s1.split()
+s2=s2.split()
 c=0
-for i in a:
-    c+=b.count(i)
+for i in s1:
+    if i in s2:
+        c+=1
 print(c)
