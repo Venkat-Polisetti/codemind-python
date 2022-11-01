b9c9f517a0c1d9fbc173754182332c06be5855ac
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-11-01 23:18:22 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() t=s1.split() u=s2.split() for i in u:     if i in t:         print(i,end=" ")

--- common_words.py
+++ common_words.py
@@ -1,9 +1,9 @@
-a=input()
-a=a.lower()
-a=a.split(' ')
-b=input()
-b=b.lower()
-b=b.split(' ')
-for i in b:
-    if i in a:
-        print(i,end=' ')
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+t=s1.split()
+u=s2.split()
+for i in u:
+    if i in t:
+        print(i,end=" ")
